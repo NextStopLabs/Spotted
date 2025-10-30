e333454b5342eedd0f797bdcdbb580ac4b882cfd
Kai <Kaiboucher2007@gmail.com>
2025-10-30 13:38:01 +0000

    updates

--- Spotted/urls.py
+++ Spotted/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from main.views import go_to_home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('mod/', include('mods.urls')),
     path('oidc/', include('mozilla_django_oidc.urls')),
+    path('accounts/profile/', go_to_home, name='profile'),
     path('', include('main.urls')),
 ]
 
